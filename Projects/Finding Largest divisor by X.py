866b38ff5627a17d54f3f482c9a437bbc524a3de
Praveen <37008507+Hacker-Pravii@users.noreply.github.com>
2021-07-02 13:58:55 +0530

    Add files via upload

---
+++ Projects/Finding Largest divisor by X.py
@@ -0,0 +1,31 @@
+num = int(input("Enter Number of elments:"))
+temp=num
+largest=0
+check=0
+number=list()
+while temp>=1:
+   numb= int(input("Enter Elements:"))
+   number.append(numb)
+   temp-=1
+
+divisor=int(input("Enter Divisible X input"))
+for i in range(0,len(number)):
+            if number[i]%divisor==0:
+                for j in range(1,len(number)):
+                    if number[i]>number[j]:
+                        largest=number[i]
+                        check=1
+                    else:
+                        largest=number[j]
+                        number[i]=number[j]
+                        check=1
+                
+            else:
+                break
+
+if largest>=1:
+        print("The Largest Number Divisable By X is",largest)
+else:
+        print("-1")
+if check!=1:
+    print("-1 Any number is not divisable by X")
